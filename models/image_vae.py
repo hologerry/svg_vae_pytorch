05d49bc089413914a46d50f1b8531771d4517c13
Gerry Gao <hologerry@gmail.com>
2020-05-24 22:32:50 +0800

    update

--- models/image_vae.py
+++ models/image_vae.py
@@ -129,7 +129,7 @@ class ImageVAE(nn.Module):
     def reparameterize(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
         # return torch.normal(mu, std)
-        esp = torch.randn(*mu.size())
+        esp = torch.randn(*mu.size(), device=mu.device)
         z = mu + std * esp
         return z
 
